09d3e5d5f2621b8d4a3005d365619126bd8a68ad
gabrielmorete <gabrielmorete@usp.br>
2019-10-07 23:19:49 -0300

    UVa

--- OJ/UVa/10271 - Chopsticks.cpp
+++ OJ/UVa/10271 - Chopsticks.cpp
@@ -36,26 +36,26 @@ ll sticks[5555];
 ll memo[5555][1123];
 ll n,k;
 
-ll pd( int pos, int rk) {
-	if (rk == 0)
-		return 0;
+ll pd() {
+	for (int i = 0; i <= k; i++)
+		memo[n-1][i] = INF, memo[n][i] = INF;
 
-	if (pos >= n-1)
-		return INF;
-	
-	ll &pdm = memo[pos][rk];
-	if (pdm != -1)
-		return pdm;
-	
-	pdm = pd( pos + 1, rk);
-	
-	if (pos > 3*(k-rk))
-	pdm = min( pdm, pd( pos + 2, rk - 1) + (sticks[pos] - sticks[pos+1])*(sticks[pos] - sticks[pos+1]));
+	for (int i = 0; i <= n; i++)
+		memo[i][0] = 0;
 
-	return pdm;
+	for (int pos = n-2; pos >= 0; pos--) {
+		for (int rk = k; rk >= 1; rk--) {
+			memo[pos][rk] = memo[pos + 1][rk];
+			if (pos > 3*(k-rk))
+				memo[pos][rk] = min( memo[pos][rk], memo[pos + 2][rk - 1] +
+					(sticks[pos] - sticks[pos+1])*(sticks[pos] - sticks[pos+1]));
+		}
+	}
+	return memo[0][k];
 }
 
 void solve() {
+	//ms( memo,-1); //Only in memoized version
 	scanf( "%d %d", &k, &n);
 	k += 8;
 
@@ -64,8 +64,8 @@ void solve() {
 
 	sort( sticks, sticks + n, greater<ll>());
 
-	ms( memo,-1);
-	cout<<pd( 0, k)<<endl;
+	
+	cout<<pd()<<endl;
 }
 
 int main() {
@@ -74,3 +74,22 @@ int main() {
 	while (t--) 
 		solve();
 }
+// Memoized Version
+// ll pd( int pos, int rk) {
+// 	if (rk == 0)
+// 		return 0;
+
+// 	if (pos >= n-1)
+// 		return INF;
+	
+// 	ll &pdm = memo[pos][rk];
+// 	if (pdm != -1)
+// 		return pdm;
+	
+// 	pdm = pd( pos + 1, rk);
+	
+// 	if (pos > 3*(k-rk))
+// 	pdm = min( pdm, pd( pos + 2, rk - 1) + (sticks[pos] - sticks[pos+1])*(sticks[pos] - sticks[pos+1]));
+
+// 	return pdm;
+// }
