61db4119cf9e5c82c786cac837294c7947ded4d5
gabrielmorete <gabrielmorete@usp.br>
2019-11-10 15:56:02 -0300

    cf

---
+++ OJ/CodeForces/9C - Hexadecimal's Numbers.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n)     for(int i=0;i<n;i++)
+#define frr(i,n)    for(int i=1;i<=n;i++)
+
+#define ms(x,i) memset(x,i,sizeof(x))
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+//cout << setprecision(9)<<fixed;
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = (long long)(1e18) + 100;   
+const int MAXN = 100100;
+const int MAXM = 1001000;
+const ll mod=1e9+7;
+const int LMAXN = 25;
+
+int main(){
+	fastio;
+	int n;
+
+	cin>>n;
+
+	int ans = 0, aux1, aux2, pwr;
+	for (int i = 1; i <= 512; i++){
+		aux1 = i;
+		aux2 = 0;
+		pwr = 1;
+		while (aux1 > 0) {
+			aux2 += (aux1%2)*pwr;
+			aux1 /= 2;
+			pwr *= 10;
+		}
+		if (aux2 <= n)
+			ans++;
+	}
+	cout<<ans<<endl;
+}
