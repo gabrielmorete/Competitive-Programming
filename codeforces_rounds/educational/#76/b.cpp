39b1082cd2549950d5be57ac3611b9e88aa85fa3
gabrielmorete <gabrielmorete@usp.br>
2019-11-13 14:37:42 -0300

    Educational #76

---
+++ codeforces_rounds/educational/#76/b.cpp
@@ -0,0 +1,57 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n)     for(int i=0;i<n;i++)
+#define frr(i,n)    for(int i=1;i<=n;i++)
+
+#define ms(x,i) memset(x,i,sizeof(x))
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+//cout << setprecision(9)<<fixed;
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = (long long)(1e18) + 100;   
+const int MAXN = 100100;
+const int MAXM = 1001000;
+const ll mod=1e9+7;
+const int LMAXN = 25;
+
+int main(){
+	fastio;
+	int t, x, y;
+	cin>>t;
+	while(t--){
+		cin>>x>>y;
+		if(x >= y){
+			cout<<"YES"<<endl;
+			continue;
+		}
+
+		if(x > 3){
+			cout<<"YES"<<endl;
+			continue;
+		}
+		if(x == 2 and y == 3){
+			cout<<"YES"<<endl;
+			continue;
+		}
+
+		cout<<"NO"<<endl;
+	}
+}
---
+++ codeforces_rounds/educational/#76/c.cpp
@@ -0,0 +1,63 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n)     for(int i=0;i<n;i++)
+#define frr(i,n)    for(int i=1;i<=n;i++)
+
+#define ms(x,i) memset(x,i,sizeof(x))
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+//cout << setprecision(9)<<fixed;
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = (long long)(1e18) + 100;   
+const int MAXN = 100100;
+const int MAXM = 1001000;
+const ll mod=1e9+7;
+const int LMAXN = 25;
+
+int n, v[212345];
+
+int main(){	
+	fastio;
+	int t;
+	cin>>t;
+	while(t--){
+		int best = INF;
+		cin>>n;
+		frr(i,n)
+			v[i] = -1;
+
+		int x;	
+		frr(i,n){
+			cin>>x;
+			if (v[x] == -1)
+				v[x] = i;
+			else{
+				best = min(best,i - v[x] + 1);
+				v[x] = i;
+			}
+		}
+
+		if (best == INF)
+			cout<<-1<<endl;
+		else		
+			cout<<best<<endl;	
+	}
+}
