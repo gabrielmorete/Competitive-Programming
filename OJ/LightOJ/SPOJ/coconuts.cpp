64578bb28b70588597d3a96ca292774671b7e45d
gabrielmorete <gabrielmorete@usp.br>
2019-12-04 23:48:32 -0300

    a

---
+++ OJ/LightOJ/SPOJ/coconuts.cpp
@@ -0,0 +1,136 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n)     for(int i=0;i<n;i++)
+#define frr(i,n)    for(int i=1;i<=n;i++)
+
+#define ms(x,i) memset(x,i,sizeof(x))
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = 0x3f3f3f3f3f3f3f;  
+const int MAXN=100100;
+const int MAXM=1001001;
+const ll mod=1e9+7;
+const int LMAXN = 20;
+
+
+int ned;
+int first[MAXN],to[MAXM],nxt[MAXM],work[MAXN],q[MAXN];
+int dist[MAXN],cap[MAXM],id[400][400];
+
+void init() {
+	ms( first, -1);
+	ned = 0;
+	fr(i,400)
+		fr(j,400)
+			id[i][j] = -2;
+}
+
+void add(int u, int v, int f) {
+	id[u][v] = ned;
+	to[ned] = v, cap[ned] = f;
+	nxt[ned] = first[u];
+	first[u] = ned++;
+	
+	to[ned] = u, cap[ned] = 0;
+	nxt[ned] = first[v];
+	first[v] = ned++;
+}
+
+int dfs(int u, int f, int t) {
+	if (u == t) return f;
+	for(int &e = work[u]; e != -1; e = nxt[e]) {
+		int v = to[e];
+		if (dist[v] == dist[u] + 1 && cap[e] > 0) {
+			int df = dfs(v, min(f, cap[e]), t);
+			if (df > 0) {
+				cap[e] -= df;
+				cap[e^1] += df;
+				return df;
+			}
+		}
+	}
+	return 0;
+}
+
+bool bfs(int s, int t) {
+	memset(&dist, -1, sizeof dist);
+	dist[s] = 0;
+	int st = 0, en = 0;
+	q[en++] = s;
+	while (en > st) {
+		int u = q[st++];
+		for(int e = first[u]; e!=-1; e = nxt[e]) {
+			int v = to[e];
+			if (dist[v] < 0 && cap[e] > 0) {
+				dist[v] = dist[u] + 1;
+				q[en++] = v;
+			}
+		}
+	}
+	return dist[t] >= 0;
+}
+
+int dinic(int s, int t) {
+	int result = 0, f;
+	while (bfs(s, t)) {
+		memcpy(work, first, sizeof work);
+		while (f = dfs(s, INF, t)) result += f;
+	}
+	return result;
+}
+
+
+void solve( int n, int m) {
+	init();
+	int src, snk;
+	src = 0;
+	snk = n + 1;
+	
+	int x;
+	frr(i,n){
+		cin>>x;
+		if (x)
+			add( src, i, 1);
+		else
+			add( i, snk, 1);
+	}
+
+	int a, b;
+	fr(i,m) {
+		cin>>a>>b;
+		add( a, b, 1);
+		add( b, a, 1);
+	}
+
+	int ans = dinic( src, snk);
+
+	cout<<ans<<endl;	
+}
+
+int main() {
+	int n, m;
+	cin>>n>>m;
+	while (n != 0) {
+		solve( n,m);
+		cin>>n>>m;
+	}
+}
