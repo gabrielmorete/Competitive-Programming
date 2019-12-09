ce03e059e8edaf2027718301819f51c49d5b80b5
gabrielmorete <gabrielmorete@usp.br>
2019-12-09 16:35:16 -0300

    cf

---
+++ OJ/CodeForces/271A - Beautiful Year.cpp
@@ -0,0 +1,57 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+ 
+#define fr(i,n)     for(int i=0;i<n;i++)
+#define frr(i,n)    for(int i=1;i<=n;i++)
+ 
+#define ms(x,i) memset(x,i,sizeof(x))
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+ 
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+//cout << setprecision(9)<<fixed;
+ 
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+ 
+const int INF = 0x3f3f3f3f;
+const ll llINF = (long long)(1e18) + 100;   
+const int MAXN = 100100;
+const int MAXM = 1001000;
+const ll mod=1e9+7;
+const int LMAXN = 25;
+ 
+bool distinct(int val){
+	bool freq[11];
+	ms(freq, 0);
+ 
+	while (val > 0){
+		if(freq[val%10])
+			return false;
+		freq[val%10]=true;
+		val /= 10;
+	}
+	return true;
+}
+ 
+int main(){
+	fastio;
+	int y;
+ 
+	cin>>y;
+	y++;
+	while(!distinct(y))
+		y++;
+	cout<<y<<endl;
+}
