c57ed0958ec0ef0e660da540b2495ee19445177d
gabrielmorete <gabrielmorete@usp.br>
2019-12-16 22:29:12 -0300

    Speed Baguncinha 5

---
+++ OJ/CodeForces/498A - Crazy Town.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n)     for(int i=0;i<n;i++)
+#define frr(i,n)    for(int i=1;i<=n;i++)
+
+#define ms(x,i) memset(x,i,sizeof(x))
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+//cout << setprecision(9)<<fixed;
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = (long long)(1e18) + 100;   
+const int MAXN = 100100;
+const int MAXM = 1001000;
+const ll mod=1e9+7;
+const int LMAXN = 25;
+
+int sng(ll x){ return x < 0 ? -1 : 1;}
+
+
+int main(){
+	fastio;
+	ll x1, x2, y1, y2, n, a, b, c, ans = 0;
+	cin>>x1>>y1>>x2>>y2>>n;
+
+	fr(i,n){
+		cin>>a>>b>>c;
+		if (sng(a*x1 + b*y1 + c) != sng(a*x2 + b*y2 + c))
+			ans++;
+	}
+	cout<<ans<<endl;
+}
