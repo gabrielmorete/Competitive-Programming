b61e43a2a80cfb020426390b8abe2d1fca1b616f
gabrielmorete <gabrielmorete@usp.br>
2020-05-09 16:58:11 -0300

    Div4!

---
+++ OJ/CodeForces/1352C - K-th Not Divisible by n.cpp
@@ -0,0 +1,63 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n)     for(int i=0;i<n;i++)
+#define frr(i,n)    for(int i=1;i<=n;i++)
+
+#define ms(x,i) memset(x,i,sizeof(x))
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+//cout << setprecision(9)<<fixed;
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = (long long)(1e18) + 100;   
+const int MAXN = 100100;
+const int MAXM = 1001000;
+const ll mod=1e9+7;
+const int LMAXN = 25;
+
+void solve(){
+	int n, k, ans;
+	cin>>n>>k;
+
+	ll mn, mx, meio;
+	mn = 1;
+	mx = 1e11; 
+	ans = n;
+	while (mn <= mx){
+		meio = (mn + mx)/2;
+
+		if (meio - meio/n >= k){
+			ans = meio;
+			mx = meio - 1;
+		}
+		else
+			mn = meio + 1;
+	}
+	cout<<ans<<endl;
+
+}
+
+int main(){
+	fastio;
+	int t;
+	cin>>t;
+	while (t--)
+		solve();
+}
