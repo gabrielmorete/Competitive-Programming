ab8b850bdd85a1bbd3dc94228c16c28ac5fa653f
gabrielmorete <gabrielmorete@usp.br>
2020-06-17 01:41:02 -0300

    sum

---
+++ OJ/UVa/10302 - Summation of Polynomials.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define fst first
+#define snd second
+
+#define fr(i,n)     for (int i = 0; i < n; i++)
+#define frr(i,n)    for (int i = 1; i <= n; i++)
+
+#define endl '\n'
+#define gnl cout << endl
+#define chapa cout << "oi meu chapa" << endl
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+//cout << setprecision(9)<<fixed;
+
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = (long long)(1e18) + 100;   
+const int MAXN = 5e4 + 10;
+
+ll sum[MAXN];
+
+int32_t main(){
+	fastio;
+	for (ll i = 1; i < MAXN; i++)
+		sum[i] = sum[i - 1] + i*i*i;
+
+	int n;
+	while (cin>>n)
+		cout<<sum[n]<<endl;
+}
