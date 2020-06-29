42bc4203ca1401e7c9690bf5d90c8ce8a120a7d8
gabrielmorete <gabrielmorete@usp.br>
2020-06-29 14:29:32 -0300

    Dinitz for maximum flow

---
+++ Caderninho/Graphs/dinic.cpp
@@ -0,0 +1,74 @@
+// Algoritmo de Dinitz
+// Caso geral : O(EV^2)
+// Matching bip : O(EV^0.5)
+// Arestas são guardadas como uma lista ligada
+// first[v] é a primeira aresta, percorrer até -1 (v = nxt[v])
+
+#define INF 0x3f3f3f3f
+const int MAXN = 1e5 + 3000;
+const int MAXM = 9e5;
+using namespace std;
+
+int ned, first[MAXN], work[MAXN], dist[MAXN], q[MAXN];
+int cap[MAXM], to[MAXM], nxt[MAXM];
+
+void init(){
+   memset(first, -1, sizeof first);
+   ned = 0;
+}
+
+void add(int u, int v, int f){
+	to[ned] = v, cap[ned] = f;
+	nxt[ned] = first[u];
+	first[u] = ned++;
+	
+	to[ned] = u, cap[ned] = 0;
+	nxt[ned] = first[v];
+	first[v] = ned++;
+}
+
+int dfs(int u, int f, int t){
+	if (u == t) return f;
+	for (int &e = work[u]; e != -1; e = nxt[e]){
+		int v = to[e];
+		if (dist[v] == dist[u] + 1 && cap[e] > 0){
+			int df = dfs(v, min(f, cap[e]), t);
+			if (df > 0){
+				cap[e] -= df;
+				cap[e^1] += df;
+				return df;
+			}
+		}
+	}
+	return 0;
+}
+
+bool bfs(int s, int t){
+	memset(&dist, -1, sizeof dist);
+	dist[s] = 0;
+	int st = 0, en = 0;
+	q[en++] = s;
+	while (en > st){
+		int u = q[st++];
+		for (int e = first[u]; e != -1; e = nxt[e]){
+			int v = to[e];
+			if (dist[v] < 0 && cap[e] > 0){
+				dist[v] = dist[u] + 1;
+				q[en++] = v;
+			}
+		}
+	}
+	return dist[t] >= 0;
+}
+
+int dinic(int s, int t){
+	int flow = 0, f;
+	while (bfs(s, t)){
+		memcpy(work, first, sizeof work);
+		while (f = dfs(s, INF, t)) 
+			flow += f;
+	}
+	return flow;
+}
+
+// Basado em : https://github.com/splucs/Competitive-Programming/tree/master/Macac%C3%A1rio
