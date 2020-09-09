833a7d050ad14cbb4b413db818e9320bbf502795
Gabriel Morete <gabrielmorete@usp.br>
2020-09-08 23:59:50 -0300

    maxflow ans residual network

--- OJ/UVa/12272 - Fake scoreboard.cpp
+++ OJ/UVa/12272 - Fake scoreboard.cpp
@@ -2,65 +2,63 @@
 using namespace std;
 
 #define pb push_back
-#define mp make_pair
 #define fst first
 #define snd second
 
-#define fr(i,n)     for(int i=0;i<n;i++)
-#define frr(i,n)    for(int i=1;i<=n;i++)
+#define fr(i,n)     for (int i = 0; i < n; i++)
+#define frr(i,n)    for (int i = 1; i <= n; i++)
 
-#define ms(x,i) memset(x,i,sizeof(x))
-#define dbg(x)  cout << #x << " = " << x << endl
-#define all(x)  x.begin(),x.end()
-
+#define endl '\n'
 #define gnl cout << endl
 #define chapa cout << "oi meu chapa" << endl
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+//cout << setprecision(9)<<fixed;
+
+#define dbg(x)  cout << #x << " = " << x << endl
+#define all(x)  x.begin(),x.end()
 
 typedef long long int ll;
 typedef pair<int,int> pii;
 typedef vector<int> vi;
-typedef vector<ll> vll;
 typedef vector<pii> vii;
-typedef pair<ll,ll> pll;
 
 const int INF = 0x3f3f3f3f;
-const ll llINF = 0x3f3f3f3f3f3f3f;  
-const int MAXN=100100;
-const int MAXM=1001001;
-const ll mod=1e9+7;
-const int LMAXN = 20;
+const ll llINF = (long long)(1e18) + 100;   
 
-int ned;
-int first[MAXN],work[MAXN],dist[MAXN],q[MAXN];
-int to[MAXM],cap[MAXM],nxt[MAXM], id[1010][1010];
+const int MAXN = 1e5 + 3000;
+const int MAXM = 9e5;
+
+int ned, first[MAXN], work[MAXN], dist[MAXN], q[MAXN];
+int cap[MAXM], to[MAXM], nxt[MAXM];
+
+int adj[300][300];
+
 void init(){
-	ned=0;
-	memset(first,-1,sizeof first);
+   memset(first, -1, sizeof first);
+   ned = 0;
 }
 
 void add(int u, int v, int f){
-
-	id[u][v]=ned;
+	to[ned] = v, cap[ned] = f;
+	nxt[ned] = first[u];
+	first[u] = ned++;
 	
-	to[ned]=v,cap[ned]=f;
-	nxt[ned]=first[u],first[u]=ned++;
-	
-	to[ned]=u,cap[ned]=0;
-	nxt[ned]=first[v],first[v]=ned++;
+	adj[u][v] = ned - 1;
 
+	to[ned] = u, cap[ned] = 0;
+	nxt[ned] = first[v];
+	first[v] = ned++;
 }
 
-int dfs(int u, int s, int t, int f){
-	if(u==t)	return f;
-	int df,v;
-	for(int& e = work[u];e!=-1;e=nxt[e]){
-		v=to[e];
-		if(dist[v]==1+dist[u] && cap[e]>0){
-			df=dfs(v,s,t,min(cap[e],f));
-			if(df>0){
-				cap[e]-=df;
-				cap[e^1]+=df;
+int dfs(int u, int f, int t){
+	if (u == t) return f;
+	for (int &e = work[u]; e != -1; e = nxt[e]){
+		int v = to[e];
+		if (dist[v] == dist[u] + 1 && cap[e] > 0){
+			int df = dfs(v, min(f, cap[e]), t);
+			if (df > 0){
+				cap[e] -= df;
+				cap[e^1] += df;
 				return df;
 			}
 		}
@@ -69,132 +67,109 @@ int dfs(int u, int s, int t, int f){
 }
 
 bool bfs(int s, int t){
-	int u,v;
-	queue<int> q;q.push(s);
-	memset(dist,-1,sizeof dist);
-	dist[s]=0;
-	while(!q.empty()){
-		u=q.front();q.pop();
-		for(int e=first[u];e!=-1;e=nxt[e]){
-			v=to[e];
-			if(cap[e]>0 && dist[v]==-1){
-				dist[v]=1+dist[u];
-				q.push(v);
+	memset(dist, -1, sizeof dist);
+	dist[s] = 0;
+	int st = 0, en = 0;
+	q[en++] = s;
+	while (en > st){
+		int u = q[st++];
+		for (int e = first[u]; e != -1; e = nxt[e]){
+			int v = to[e];
+			if (dist[v] < 0 && cap[e] > 0){
+				dist[v] = dist[u] + 1;
+				q[en++] = v;
 			}
 		}
 	}
-	return dist[t]>=0;
+	return dist[t] >= 0;
 }
 
 int dinic(int s, int t){
-	int df,result;
-	result=0;
-	while(bfs(s,t)){
-		memcpy(work,first,sizeof first);
-		while(df=dfs(s,s,t,INF))
-			result+=df;
+	int flow = 0, f;
+	while (bfs(s, t)){
+		memcpy(work, first, sizeof work);
+		while (f = dfs(s, INF, t)) 
+			flow += f;
 	}
-	return result;
+	return flow;
 }
 
-
-int team[1010], prob[1010], s1,s2,mf,cnt;
-char ans[100][100];
-
-void solve( int t, int p) {
-	init();	
-	ms(id,0);
-	
-	int source = 0;
-	int sink = p + t + 1;
-
-	s1 = s2 = mf = 0;
-
-	frr(i,t) {
-		cin>>team[i];
-		add( source, i, team[i]);
-		frr(j,p){
-			add( i, t + j, 1);
-		}
-		s1 += team[i];
-	}
-	frr(j,p) {
-		cin>>prob[j];
-		add( t + j, sink, prob[j]);
-		s2 += prob[j];
-	}
-
-	if (s1 != s2) {
-		cout << "Impossible" << endl;
-		return;
-	}
-
-	mf = dinic( source, sink);
-	
-	if (s1 > mf) {
-		cout << "Impossible" << endl;
-		return;
-	}
-
-	frr(i,t)
-		frr(j,p){
-			
-			dinic( source, sink);
-
-			int ini, meio, fim;
-			ini = id[source][i];
-			meio = id[i][t+j];
-			fim = id[t+j][sink];
-
-			if (cap[meio^1] == 0 ){
-				cap[meio] = cap[meio^1] = 0;
-				continue;
-			}
-
-			
-			cap[ini]++;
-			cap[ini^1]--;
-			
-			cap[meio] = cap[meio^1] = 0;
-
-			cap[fim]++;
-			cap[fim^1]--;
-
-			if(dinic( source, sink))
-				continue;
-			
-			cap[meio] = 1;
-			cap[meio^1] = 0;
-		}
-
-	dinic( source, sink);
-
-	frr( i, t){
-		frr( j, p){
-			int aux = id[i][t+j]^1;
-			if ( cap[aux] )
-				cout<<'Y';
-			else
-				cout<<'N';
-		}
-		gnl;	
-	}	
+char ans[300][300];
 
 
-
-
-}
-
-int main(){
+int32_t main(){
 	fastio;
-	int t,p;
-	bool flag=false;
-	while(true){
-		cin >> t >> p;
-		if(!t || !p)	break;
-		if(flag)	gnl;
-		solve( t,p);
-		flag=true;
+	int t, p, st, sp, src, snk;
+
+	cin>>t>>p;
+	while (t != 0){
+		init();
+
+		src = st = sp = 0;
+		snk = t + p + 3;
+
+		int x;
+		frr(i, t){
+			cin>>x;
+			st += x;
+			add(src, i, x);
+		}
+
+		frr(j, p){
+			cin>>x;
+			sp += x;
+			add(j + t, snk, x);
+		}
+
+		frr(i, t)
+			frr(j, p)
+				add(i, j + t, 1);
+
+		int mf = dinic(src, snk);		
 		
+
+		if (sp != st or mf != sp){
+			cout<<"Impossible"<<endl;
+		}
+		else{
+			int a, b, c;
+			for (int i = 1; i <= t; i++) // percorre em ordem lexico
+				for (int j = 1; j <= p; j++){
+					a = adj[src][i];
+					b = adj[i][j + t];
+					c = adj[j + t][snk];
+
+					if (cap[b] > 0){
+						cap[b] = cap[b ^ 1] = 0;
+						ans[i][j] = 'N';
+						continue;
+					}
+
+					// diminui fluxo em 1
+					cap[a]++;
+					cap[a^1]--;
+					cap[b] = cap[b^1] = 0;
+					cap[c]++;
+					cap[c ^ 1]--;
+					// checa se há um caminho que não usa aresta
+					if (dinic(src, snk) == 0){//não aumentou
+						cap[a]--; // 10x mais rápido do que redar outro dinic
+						cap[c]--;
+						ans[i][j] = 'Y';
+					}
+					else
+						ans[i][j] = 'N';
+				}
+
+			frr(i, t){
+				frr(j, p)
+					cout<<ans[i][j];
+				gnl;	
+			}			
+		}
+		
+		cin>>t>>p;
+		if (t != 0)
+			gnl;
 	}
 }
