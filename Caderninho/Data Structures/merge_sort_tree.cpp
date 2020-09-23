7cbf0ee39aad3926343dcdafd70c9c69bb809fe6
Gabriel Morete <gabrielmorete@usp.br>
2020-09-22 21:05:38 -0300

    merge sort tree + kth-element

--- Caderninho/Data Structures/merge_sort_tree.cpp
+++ Caderninho/Data Structures/merge_sort_tree.cpp
@@ -1,9 +1,11 @@
 // Merge sort tree
 // Encontra o número de elementos <= k no intervalo [l, r] 
+// Encontra o k-ésimo elemento no intervalo [l, r] 
 // complexidade:
-// 		build : O(nlog(n))
-//		query : O(log(n)^2)
-//		memória : O(nlog(n)
+// 		build 	  :	O(nlog(n))
+//		query 	  : O(log(n)^2)
+//		query_kth : O(log(n)^2) 
+//		memória	  : O(nlog(n)
 
 #include "bits/stdc++.h"
 using namespace std;
@@ -11,7 +13,7 @@ using namespace std;
 #define all(x)  x.begin(), x.end()
 const int MAXN = 1e5 + 10;
 
-vector<int> v; // vetor de elementos
+int v[MAXN]; // vetor de elementos (vetor de pair para kth-element)
 vector<int> tree[4 * MAXN];
 
 void build(int node, int l, int r){
@@ -29,4 +31,25 @@ int query(int node, int l, int r, int ql, int qr, int val){
 		return lower_bound(all(tree[node]), val + 1) - tree[node].begin();
 	int meio = (l + r)/2;
 	return query(node<<1, l, meio, ql, qr, val) + query(node<<1|1, meio + 1, r, ql, qr, val);
+}
+
+// Para consultar k-ésimo elemento em [l:r] modifique a build com
+// if (l == r) return tree[node].push_back(v[l].snd);
+// em que v = {valor, indice} é um vetor ordenado contendo os
+// elementos do vetor original e seus indices originais.
+// A função retorna a posição original do k-ésimo elemento e não
+// o seu valor.
+
+#error ordene o vetor {valor, indice}	
+// query_kth(1, 1(0), n(n-1), ql, qr, val) devolve o k-ésimo elemento em v[l:r]
+int query_kth(int node, int l, int r, int ql, int qr, int val){
+	if (l == r)	return tree[node][0]; // posição original do k-ésimo elemento
+	int fst = lower_bound(all(tree[node<<1]), ql) - tree[node<<1].begin(); // filho esquerdo
+	int lst = lower_bound(all(tree[node<<1]), qr + 1) - tree[node<<1].begin();
+	int meio = (l + r)/2;
+	int amnt = lst - fst; // # de elementos no filho esquerdo
+	if (amnt >= val) // val está no filho esquerdo
+		return query_kth(node<<1, l, meio, ql, qr, val);
+	else
+		return query_kth(node<<1|1, meio + 1, r, ql, qr, val - amnt);
 }
