cf96f31ad56e7051350a1e89dc6c3ccb71d49dff
Gabriel Morete <gabrielmorete@usp.br>
2021-01-12 00:43:35 -0300

    max cost matching

--- OJ/SPOJ/scities.cpp
+++ OJ/SPOJ/scities.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define fr(i,n)     for (int i = 0; i < n; i++)
 #define frr(i,n)    for (int i = 1; i <= n; i++)
 
-//#define endl '\n'
+#define endl '\n'
 #define gnl cout << endl
 #define chapa cout << "oi meu chapa" << endl
 
@@ -20,84 +20,123 @@ typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<pii> vii;
 
-const int INF = 0x3f3f3f3f;
 const ll llINF = (long long)(1e18) + 100;   
-const int MAXN = 1e2 + 10;
-const int MAXM = 1e2 + 10;
 
+using coord = long long int; // tipo de dados
 
+const int MAXN = 110;
+const coord INF = 0; //para maximização INF = 0 e negue custos
 
+bool zero(coord x){ return x == 0; }// x == 0, para int/ll
 
-// Hungarian's Algorithm O(n^2 m)
-// linhas de 1 a n, colunas de 1 a n
-// codigo de minimização, para maximização use -custo
+int N, matchl[MAXN], matchr[MAXN];
+coord cst[MAXN][MAXN];
 
-int n, m; // faça m >= n
-int pu[MAXN], pv[MAXN], cost[MAXN][MAXM];
-int pairV[MAXN], way[MAXM], minv[MAXM];
-bool used[MAXM];
-
-void hungarian() {
-	memset(&pairV, 0, sizeof pairV);
-	for(int i = 1, j0 = 0; i <= n; i++) {
-		pairV[0] = i;
-		memset(&minv, INF, sizeof minv);
-		memset(&used, false, sizeof used);
-		do {
-			used[j0] = true;
-			int i0 = pairV[j0], delta = INF, j1;
-			for(int j = 1; j <= m; j++) {
-				if (used[j]) continue;
-				int cur = cost[i0][j] - pu[i0] - pv[j];
-				if (cur < minv[j])
-					minv[j] = cur, way[j] = j0;
-				if (minv[j] < delta)
-					delta = minv[j], j1 = j;
-			}
-			for(int j = 0; j <= m; j++) {
-				if (used[j])
-					pu[pairV[j]] += delta, pv[j] -= delta;
-				else minv[j] -= delta;
-			}
-			j0 = j1;
-		} while(pairV[j0] != 0);
-		do {
-			int j1 = way[j0];
-			pairV[j0] = pairV[j1];
-			j0 = j1;
-		} while(j0);
-	}
+void init(int n, int m){
+	N = max(n, m);
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cst[i][j] = (i < n and j < m) ? INF : 0;
 }
 
-void solve(){
-	cin>>n>>m;
-	for (int i = 0; i <= max(n, m) + 1; i++)
-		for (int j = 0; j <= max(n, m) + 1; j++)
-			cost[i][j] = 0;
+void add(int x, int y, coord c){ cst[x][y] = c; }
 
-	int a, b, c;	
-	cin>>a>>b>>c;
-	while (a != 0){
-		cost[a][b] = -c;
-		cin>>a>>b>>c;
-	}
+coord hungarian(){
+	int mat = 0, dad[MAXN], vis[MAXN];
+	coord ds[MAXN], y[MAXN], z[MAXN];
 
-	m = max(n, m); // igualando os tamanhos 
-	n = max(n, m);
-
-	hungarian();
+	for (int i = 0; i < N; i++)
+		y[i] = *min_element(cst[i], cst[i] + N);
 	
-	int ans = 0;
-	for (int j = 1; j <= m; j++)
-		if (pairV[j] != 0)
-			ans -= cost[pairV[j]][j];
-	cout<<ans<<endl;	
+	for (int j = 0; j < N; j++){
+		z[j] = cst[0][j] - y[0];
+		for (int i = 1; i < N; i++)
+			z[j] = min(z[j], cst[i][j] - y[i]);
+	}
+	
+	memset(matchl, -1, sizeof matchl);
+	memset(matchr, -1, sizeof matchr);
+
+	for (int i = 0; i < N; i++) // speedup
+		for (int j = 0; j < N; j++)
+			if (matchr[j] == -1 && zero(cst[i][j] - y[i] - z[j])){
+				matchl[i] = j;
+				matchr[j] = i;
+				mat++;
+				break;
+			}
+
+	while (mat < N){
+	    int s = 0, j = 0, i;
+	    while (matchl[s] != -1)
+	    	s++;
+
+	    memset(dad, -1, sizeof dad);
+	    memset(vis, 0, sizeof vis);
+
+	    for (int k = 0; k < N; k++)
+	    	ds[k] = cst[s][k] - y[s] - z[k];
+	    
+	    while (true){
+	        j = -1;
+	        for (int k = 0; k < N; k++)
+	        	if (!vis[k] && (j == -1 || ds[k] < ds[j]))
+	        		j = k;
+	        
+	        vis[j] = 1; 
+	        i = matchr[j];
+	        if (i == -1) 
+	        	break;
+
+	        for (int k = 0; k < N; k++)
+		        if (!vis[k]){
+		            auto new_ds = ds[j] + cst[i][k] - y[i] - z[k];
+		            if(ds[k] > new_ds){
+		            	ds[k] = new_ds;
+		            	dad[k] = j;
+		            }
+		        }
+	    }
+
+	    for (int k = 0; k < N; k++)
+		    if (k != j && vis[k]){
+		    	auto w = ds[k] - ds[j];
+		    	z[k] += w;
+		    	y[matchr[k]] -= w;
+		    }
+	  
+	    y[s] += ds[j];
+	  
+	    while (dad[j] >= 0){
+	    	int d = dad[j]; 
+	    	matchr[j] = matchr[d]; 
+	    	matchl[matchr[j]] = j; 
+	    	j = d;
+	    }
+
+	    matchr[j] = s; 
+	    matchl[s] = j;
+	    mat++;
+	}
+	
+	coord value = 0;
+	for (int i = 0; i < N; i++)
+		value += cst[i][matchl[i]];
+	return value;
 }
 
 int32_t main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
-	int T;
+	int T, n, m, a, b, c;
 	cin>>T;
-	while (T--)
-		solve();
+	while (T--){
+		cin>>n>>m;
+
+		init(n, m); // inicia o hungaro
+		while (cin>>a>>b>>c and a != 0)
+			add(a - 1, b - 1, -c); // maximization!!!
+		
+		cout<<-hungarian()<<'\n';
+	}
+
 }
