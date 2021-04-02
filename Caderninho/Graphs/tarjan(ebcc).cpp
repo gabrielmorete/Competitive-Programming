d8f7a23de0f70e455b16405d436ce9ae8310872b
gabrielmorete <gabrielmorete@usp.br>
2021-04-02 18:56:29 -0300

    Tarjan for 2-edge-components

--- Caderninho/Graphs/tarjan(ebcc).cpp
+++ Caderninho/Graphs/tarjan(ebcc).cpp
@@ -1,5 +1,9 @@
-// Algoritmo de Tarjan para BCC
+// Algoritmo de Tarjan para componentes aresta-biconexas (bcc)
 // complexidade : O(n + m)
+//
+// Dois vértices u e v estão na mesma bcc se há 
+// dois caminhos sem arestas em comum que ligam u e v (repete vertice)
+// Toda bcc é conexa e toda aresta dessa pertence a algum circuito
 
 #include "bits/stdc++.h"
 using namespace std;
@@ -7,24 +11,23 @@ using namespace std;
 const int MAXN = 1e5 + 10;
 
 int n, m, sn, clk, id;
-int pre[MAXN], lo[MAXN], pa[MAXN], stk[MAXN], bcc[MAXN]; // bcc[v] é a componente biconexa de v
+int pre[MAXN], lo[MAXN], stk[MAXN], bcc[MAXN]; // bcc[v] é a componente biconexa de v
 vector< pair<int,int> > adj[MAXN], adjbcc[MAXN];
 
-void dfsRebcc( int v) {
-	pre[v] = clk++;
+void dfs_ebcc(int v, int p){
+	lo[v] = pre[v] = clk++;
 	stk[sn++] = v;
-	lo[v] = pre[v];
-	for (auto x : adj[v]) {
-		int  w = x.fst;
-		if (pre[w] == -1) { 
-			pa[w] = v; 
-			dfsRebcc( w); 
-			lo[v] = min( lo[v], lo[w]);
+
+	for (auto x : adj[v]){
+		int w = x.first; // vertex id
+		if (pre[w] == -1){ 
+			dfs_ebcc(w, v); 
+			lo[v] = min(lo[v], lo[w]);
 		} 
-		else if (w != pa[v]) 
-			lo[v] = min( lo[v], pre[w]); 
+		else if (w != p) 
+			lo[v] = min(lo[v], pre[w]); 
 	} 
-	if (lo[v] == pre[v]) { 
+	if (lo[v] == pre[v]){ 
 		int u;
 		do {
 			u = stk[--sn];
@@ -34,19 +37,17 @@ void dfsRebcc( int v) {
 	}
 }
 
-int ebcc() {
-	for (int v = 0; v < n; v++) // 0 indexed
-		pre[v] = -1;
+int ebcc(){
+	fill(pre, pre + n + 1, -1);
 	sn = clk = id = 0;
-	for (int v = 0; v < n; v++)
-		if (pre[v] == -1) { 
-			pa[v] = v;
-			dfsRebcc( v);
-		}
+	for (int v = 0; v < n; v++) // 0 indexed
+		if (pre[v] == -1)
+			dfs_ebcc(v, v);
+		
 	return id;
 }
 
-// Monta arvore de BCC
+// Monta a árvore de BCC
 void build_ebcc_graph() {
 	for (int v = 0; v < n; v++)
 		for (auto x : adj[v])
