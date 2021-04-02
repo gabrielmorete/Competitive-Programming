c1fbf97bec8f36cc8d15827a073ff003dbf43565
gabrielmorete <gabrielmorete@usp.br>
2021-04-02 19:17:28 -0300

    Tarjan for strong components

--- Caderninho/Graphs/tarjan(scc).cpp
+++ Caderninho/Graphs/tarjan(scc).cpp
@@ -1,5 +1,7 @@
-// Algoritmo de Tarjan para SCC
+// Algoritmo de Tarjan para componentes fortemente conexas (scc)
 // complexidade : O(n + m)
+//
+// Vértices u e v estão na mesma compoente se há caminho u->v e v->u
 
 #include "bits/stdc++.h"
 using namespace std;
@@ -11,15 +13,15 @@ int n, clk, sn, id;
 int pre[MAXN], lo[MAXN], stk[MAXN], scc[MAXN]; // scc[v] é a componente forte de v
 vector<int> adj[MAXN], adjscc[MAXN];
 
-void dfsRscc( int v) {
+void dfs_scc(int v){
 	pre[v] = lo[v] = clk++;
 	stk[sn++] = v;
-	for (auto x : adj[v]) {
+	for (auto x : adj[v]){
 		if (pre[x] == -1)
-			dfsRscc( x);
-		lo[v] = min( lo[v], lo[x]);
+			dfs_scc(x);
+		lo[v] = min(lo[v], lo[x]);
 	}
-	if (lo[v] == pre[v]) {
+	if (lo[v] == pre[v]){
 		int u;
 		do {
 			u = stk[--sn];
@@ -30,18 +32,17 @@ void dfsRscc( int v) {
 	}
 }
 
-int tarjan() {
-	for (int v = 0; v < n; v++)
-		pre[v] = -1;
+int findscc(){
+	fill(pre, pre + n + 1, -1);
 	clk = sn = id = 0;
-	for (int v = 0; v < n; v++)
+	for (int v = 0; v < n; v++) // 0 indexed
 		if (pre[v] == -1)
-			dfsRscc( v);
+			dfs_scc(v);
 	return id;	
 }
 
-// Constoi arvore das scc
-void build_scc_graph() {
+// constoi arvore das scc
+void build_scc_graph(){
 	for (int v = 0; v < n; v++)
 		for (auto x : adj[v])
 			if (scc[v] != scc[x])
