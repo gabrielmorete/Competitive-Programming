d56a9483b4c0831a855e60015c387059825586bb
gabrielmorete <gabrielmorete@usp.br>
2021-06-25 22:12:32 -0300

    string hashing

---
+++ Caderninho/Strings/string_hashing.cpp
@@ -0,0 +1,40 @@
+// String Hashing
+// Complexidade:
+// 		str_hash : O(|s|)
+// 		operator() : O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+const ll mod1 = 1000015553; // primo grande 1
+const ll mod2 = 1000028537; // primo grande 2
+// const ll mod3 = 1000041323; // primo grande 3 coisa ta feia
+
+mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count()); // Gerador aleatório
+static ll p = uniform_int_distribution<int>(27, mod1 - 1)(rng);	// Caso TL usa const p
+
+struct str_hash{
+	string s;
+	ll n;
+	vector<ll> hsh1, pwr1, hsh2, pwr2;
+
+	str_hash(string _s) : n(_s.size()), s(_s), hsh1(n), pwr1(n), hsh2(n), pwr2(n){	
+		pwr1[0] = pwr2[0] = 1;
+		for (int i = 1; i < n; i++){
+			pwr1[i] = (p * pwr1[i - 1]) % mod1;
+			pwr2[i] = (p * pwr2[i - 1]) % mod2;
+		}
+		hsh1[0] = hsh2[0] = s[0];
+		for (int i = 1; i < n; i++){
+			hsh1[i] = (hsh1[i - 1] * p + (ll)(s[i]))%mod1;
+			hsh2[i] = (hsh2[i - 1] * p + (ll)(s[i]))%mod2;
+		}
+	}
+	pair<ll, ll> operator()(int i, int j){ // hash no intervalo [i, j]
+		if (i == 0) return {hsh1[j], hsh2[j]};
+		ll ret1 = ((hsh1[j] - (hsh1[i - 1] * pwr1[j - i + 1])) % mod1 + mod1) % mod1;
+		ll ret2 = ((hsh2[j] - (hsh2[i - 1] * pwr2[j - i + 1])) % mod2 + mod2) % mod2;
+		return {ret1, ret2};
+	}
+};
